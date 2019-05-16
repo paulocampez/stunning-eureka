5d38388fa0613be0e3dc036ba5d8063712eab528
METAORA\paulo.campez <prcampez@gmail.com>
2019-05-16 15:16:49 -0300

    Criado projeto de biblioteca Model, Criado metodo de teste para buscar api

--- Stunnig.API/Stunning.MVC/Controllers/HomeController.cs
+++ Stunnig.API/Stunning.MVC/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Stunning.MVC.Models;
+using Stunning.Model;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 
 namespace Stunning.MVC.Controllers
 {
@@ -12,7 +16,30 @@ namespace Stunning.MVC.Controllers
     {
         public IActionResult Index()
         {
+            var teste = GetFuncionarios();
             return View();
+        }
+
+        public static List<Funcionarios> GetFuncionarios()
+        {
+            List<Funcionarios> lstFuncionarios = new List<Funcionarios>();
+            string url = "http://localhost:59279/api/Home/";
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(url);
+            getRequest.Method = "GET";
+            try
+            {
+                var getResponse = (HttpWebResponse)getRequest.GetResponse();
+                Stream newStream = getResponse.GetResponseStream();
+                StreamReader sr = new StreamReader(newStream);
+                var result = sr.ReadToEnd();
+                lstFuncionarios = JsonConvert.DeserializeObject<List<Funcionarios>>(result);
+
+                return lstFuncionarios;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("erro api");
+            }
         }
 
         public IActionResult Privacy()
---
+++ Stunnig.API/Stunning.Model/Funcionarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stunning.Model
+{
+    public class Funcionarios
+    {
+        public int? IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime? DataCad { get; set; }
+        public string Cargo { get; set; } //TODO: Enum
+        public string UfNasc { get; set; }
+        public decimal? Salario { get; set; }
+        public string Status { get; set; } //TODO: Enum
+    }
+}
